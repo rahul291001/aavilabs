de2ea29ff7168c4d2451be085421526e1eced96e
pavan/1130 <bpavankumar516@gmail.com>
2023-10-10 13:28:17 +0530

    first commit

--- server/db.js
+++ server/db.js
@@ -1,15 +1,18 @@
 const mongoose = require("mongoose");
 
-module.exports = () => {
-	const connectionParams = {
-		useNewUrlParser: true,
-		useUnifiedTopology: true,
-	};
-	try {
-		mongoose.connect(process.env.DB, connectionParams);
-		console.log("Connected to database successfully");
-	} catch (error) {
-		console.log(error);
-		console.log("Could not connect database!");
-	}
+module.exports = async () => {
+  const connectionParams = {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  };
+
+  try {
+    await mongoose.connect(
+      "mongodb+srv://admin:11301130@cluster0.ibfabjp.mongodb.net/Form?retryWrites=true&w=majority",
+      connectionParams
+    );
+    console.log("Connected to the MongoDB successfully");
+  } catch (error) {
+    console.error("Error connecting to the MongoDB:", error);
+  }
 };
